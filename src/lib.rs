632b34f96dd84f1cf8f41789a0137ac8a0b64817
Logan Williams <logan@fireflysoftware.dev>
2026-05-02 17:10:25 +0000

    chore: initial codespace setup for kvdb

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![allow(clippy::module_name_repetitions)]
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
